5b729072ea13bdea88ee98c8dcadc8d483874ad8
dev Jeff <138620598+dev-rdj@users.noreply.github.com>
2025-07-09 21:02:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false
-    },
-    spaceBetween: 16,
-    slidesPerView: 1
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.nav-menu a').forEach(el => {
+    el.addEventListener('click', e => {
+      e.preventDefault();
+      const href = el.getAttribute('href');
+      document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+    });
   });
 });
