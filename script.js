94b489bdcb9e68b851256fd8f3e267700c40daa1
dev Jeff <138620598+dev-rdj@users.noreply.github.com>
2025-07-09 14:47:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Aura Interiors Website Ready");
-
   const swiper = new Swiper('.swiper-container', {
     loop: true,
     pagination: {
       el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      clickable: true
     },
     autoplay: {
       delay: 3000,
-      disableOnInteraction: false,
+      disableOnInteraction: false
     },
-    spaceBetween: 20,
-    slidesPerView: 1,
-    breakpoints: {
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-    }
+    spaceBetween: 16,
+    slidesPerView: 1
   });
 });
